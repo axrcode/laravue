263102c7ec0c9004eeb89dbf70c696303505b868
axrcode <axrcode@gmail.com>
2022-01-06 20:29:43 -0600

    Add app main component

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hola Mundo desde Vue.js</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Añadimos el CSS compilado -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Home</title>
+</head>
+<body>
+    <!-- Renderizamos el componente princiapl -->
+    <div id="app"></div>
+    <!-- Añadimos el JS compilado -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
