4a2e514c58e643822f2af48bebb9a498f6ef75db
axrcode <axrcode@gmail.com>
2022-01-17 22:45:57 -0600

    Add API crud basic

---
+++ app/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item;
+        $item->name = $request->name;
+        $item->quantity = $request->quantity;
+        $item->price = $request->price;
+        $item->description = $request->description;
+        $item->save();
+
+        return response()->json(['msg' => 'Producto Registrado'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item $item)
+    {
+        return response()->json($item, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Item $item)
+    {
+        $item->name = $request->name;
+        $item->quantity = $request->quantity;
+        $item->price = $request->price;
+        $item->description = $request->description;
+        $item->update();
+
+        return response()->json(['msg' => 'Producto Modificado'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+
+        return response()->json(['msg' => 'Producto Eliminado'], 200);
+    }
+}
